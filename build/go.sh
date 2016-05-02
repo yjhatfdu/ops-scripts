cd9ee01b9222f34666d42b2633f9a223c3d1031f
Riku Ayanokoji <riku.ayanokoji@gmail.com>
2016-05-02 10:00:05 +0900

    Update go.sh

--- build/go.sh
+++ build/go.sh
@@ -4,5 +4,5 @@ if [ -z $GO_VERSION ]; then
 fi
 wget https://storage.googleapis.com/golang/go$GO_VERSION.linux-amd64.tar.gz
 tar xf go$GO_VERSION.linux-amd64.tar.gz
-cp -rf go /usr/local
-ln -s /usr/local/go/bin/go /usr/bin/go
+mv go /opt
+ln -s /opt/go/bin/go /usr/bin/go

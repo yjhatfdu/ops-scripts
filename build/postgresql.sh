54627b1eb5ce23fe614d7ccb74fa6e1399adb994
yjhatfdu <yjhatfdu@users.noreply.github.com>
2017-08-05 20:47:04 +0800

    Create postgresql.sh

---
+++ build/postgresql.sh
@@ -0,0 +1,12 @@
+if [ -z $PG_VERSION ]; then
+	PG_VERSION=9.6.3
+fi
+sudo apt update
+sudo apt install -y build-essential libreadline-dev zlib1g-dev libxml2-dev python3 libpython3-dev
+wget https://ftp.postgresql.org/pub/source/v$PG_VERSION/postgresql-9.6.3.tar.bz2
+tar xvf postgresql-$PG_VERSION.tar.bz2
+cd postgresql-$PG_VERSION
+sudo ln -s python3 /usr/bin/python
+CFLAGS='-O3' ./configure --with-libxml --with-python
+make world -j16
+sudo make install-world
